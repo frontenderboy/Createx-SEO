18701674aa8f361445601b132302f728a45a9f29
Aliaksei Lahuta <frontenderboy@gmail.com>
2024-01-29 20:10:42 +0100

    feat: add freequently questions section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Tabs
 const listTabs = document.querySelectorAll('[data-tab]');
 const contents = document.querySelectorAll('[data-tab-content]');
 
@@ -17,3 +18,31 @@ listTabs.forEach(tab =>
 
 document.querySelector('[data-tab="tab-1"]').classList.add('active-tab')
 document.querySelector('#tab-1').classList.remove('hidden');
+
+
+// Accordion
+const titles = document.querySelectorAll('.accordion__title');
+const accordionContents = document.querySelectorAll('.accordion__content');
+
+titles.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tabAccordion);
+
+    if (activeContent.classList.contains('active')) {
+        activeContent.classList.remove('active');
+        item.classList.remove('active');
+        activeContent.style.maxHeight = 0;
+    } else {
+        accordionContents.forEach(element => {
+            element.classList.remove('active');
+            element.style.maxHeight = 0;
+        });
+        titles.forEach(element => element.classList.remove('active'));
+        item.classList.add('active');
+        activeContent.classList.add('active');
+        activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+}))
+
+document.querySelector('[data-tab-accordion="tabAccordion-1"]').classList.add('active');
+document.querySelector('#tabAccordion-1').classList.add('active');
+document.querySelector('#tabAccordion-1').style.maxHeight = document.querySelector('#tabAccordion-1').scrollHeight + 'px';
